53b90de641d7386a1e8c378b920ccdcea2f0765a
Justin W. Flory <git@jwf.io>
2016-08-16 05:09:21 -0400

    Update one-line script to the command I'm currently using

--- playbooks/run_gsoc.sh
+++ playbooks/run_gsoc.sh
@@ -2,3 +2,5 @@
 # Handy shortcut for a command I run frequently...
 
 ansible-playbook -i ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/inventory -e ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/group_vars/@wordpress ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/playbooks/manual/upgrade/wordpress.yml
+
+ansible-playbook -i ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/inventory -e ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/group_vars/@wordpress -b ~/Software/GSoC/ansible/playbooks/manual/wordpress-install.yml --extra-vars="target='wordpress'"
