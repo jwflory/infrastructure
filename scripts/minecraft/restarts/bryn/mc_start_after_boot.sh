2acf855c962544b843c1bc8b186af2277b6b781d
Justin W. Flory <git@jwf.io>
2017-06-23 16:52:43 -0500

    Make supybot tmux window persist after boot

--- scripts/minecraft/restarts/bryn/mc_start_after_boot.sh
+++ scripts/minecraft/restarts/bryn/mc_start_after_boot.sh
@@ -3,8 +3,10 @@
 
 tmux new-session -d -s bungeecord
 tmux new-session -d -s saikban
+tmux new-session -d -s supybot
 tmux new-session -d -s workflow
 
 tmux send-keys -t bungeecord "cd ~/servers/bungeecord && ./start.sh" Enter
 tmux send-keys -t saikban "cd ~/servers/saikban-web && ./start.sh" Enter
+tmux send-keys -t supybot "supybot limnoria/fanshawe/Fanshawe.conf" Enter
 tmux send-keys -t workflow "echo mc_start_after_boot.sh executed." Enter
