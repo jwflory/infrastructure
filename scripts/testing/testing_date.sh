bb956c1ef5a6121bdd83dd72df0e4e22f94aaac5
Justin W. Flory <git@jwf.io>
2017-06-24 00:34:56 -0500

    Remove old testing scripts from high school

--- scripts/testing/testing_date.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATE=(date +%Y-%m-%d)
-
-echo $DATE
-echo testtest$(DATE)testtest
--- scripts/testing/testing_tmux_shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# A script to test sending remote commands to tmux sessions
-
-date=$(date +A%)
-
-tmux send-keys -t survival "say Cron Test" Enter
