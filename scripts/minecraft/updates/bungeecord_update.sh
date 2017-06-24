346fe1a62644261e53ae5855bdcb75cdacc5dade
Justin W. Flory <git@jwf.io>
2017-06-23 23:07:18 -0500

    Follow Bash naming conventions

--- scripts/minecraft/updates/bungeecord_update.sh
+++ scripts/minecraft/updates/bungeecord_update.sh
@@ -3,7 +3,7 @@
 # This script is a really simple way to update to the latest version
 # of BungeeCord. It can be automated easily with cron. To use it
 # effectively, create a .minecraftrc file in your home directory with
-# the $BUNGEECORDDIR variable set to the full path of wherever your
+# the $bungeecord_dir variable set to the full path of wherever your
 # BungeeCord install lives.
 # 
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -15,5 +15,5 @@ source ~/.minecraftrc
 if [ ! -f  BungeeCord.jar ]; then
     wget https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
 fi
-rm $BUNGEECORDDIR/BungeeCord.jar
-mv BungeeCord.jar $BUNGEECORDDIR
+rm $bungeecord_dir/BungeeCord.jar
+mv BungeeCord.jar $bungeecord_dir
