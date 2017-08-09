54231274836f96feecc1b0290a6b2921fd7b18ea
Justin W. Flory <git@jwf.io>
2017-08-09 13:21:23 -0500

    Add color coding for easier viewing in stdout

--- scripts/data/backups/arguru_server_backups.sh
+++ scripts/data/backups/arguru_server_backups.sh
@@ -2,34 +2,39 @@
 # A script to backup all servers using duply
 #
 
-echo "Beginning backups..."
+GREEN='\033[0;32m'
+NC='\033[0m'
+ORANGE='\033[0;33m'
+RED='\033[0;31m'
 
-echo "Backing up global plugin directory..."
+echo -e "${ORANGE}Beginning backups...${NC}"
+
+echo -e "${ORANGE}Backing up global plugin directory...${NC}"
 duply plugindir backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up survival... (this could take a while)"
+echo -e "${ORANGE}Backing up survival... (this could take a while)${NC}"
 duply survival backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up lobby..."
+echo -e "${ORANGE}Backing up lobby...${NC}"
 duply lobby backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up creative..."
+echo -e "${ORANGE}Backing up creative...${NC}"
 duply creative backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up space..."
+echo -e "${ORANGE}Backing up space...${NC}"
 duply space backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up development..."
+echo -e "${ORANGE}Backing up development...${NC}"
 duply development backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "Backing up RITcraft Survival..."
+echo -e "${ORANGE}Backing up RITcraft Survival...${NC}"
 duply rit-survival backup
-echo "Success!"
+echo -e "${GREEN}Duply exited.${NC}"
 
-echo "All done! Please double-check file integrity."
+echo -e "${GREEN}All done! Please double-check file integrity.${NC}"
