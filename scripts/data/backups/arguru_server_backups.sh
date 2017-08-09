068e5300a4dbf82bf07006c5e42f8936ab20e54d
Justin W. Flory <git@jwf.io>
2017-08-09 13:43:26 -0500

    Retire space server, backup servers in alphabetical order

--- scripts/data/backups/arguru_server_backups.sh
+++ scripts/data/backups/arguru_server_backups.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script to backup all servers using duply
+# A script to backup all servers in alphabetical order using duply
 #
 
 GREEN='\033[0;32m'
@@ -7,34 +7,30 @@ NC='\033[0m'
 ORANGE='\033[0;33m'
 RED='\033[0;31m'
 
-echo -e "${ORANGE}Beginning backups...${NC}"
-
-echo -e "${ORANGE}Backing up global plugin directory...${NC}"
-duply plugindir backup
-echo -e "${GREEN}Duply exited.${NC}"
-
-echo -e "${ORANGE}Backing up survival... (this could take a while)${NC}"
-duply survival backup
-echo -e "${GREEN}Duply exited.${NC}"
-
-echo -e "${ORANGE}Backing up lobby...${NC}"
-duply lobby backup
-echo -e "${GREEN}Duply exited.${NC}"
+echo -e "${ORANGE}Beginning backups...${NC}\n\n\n\n\n"
 
 echo -e "${ORANGE}Backing up creative...${NC}"
 duply creative backup
-echo -e "${GREEN}Duply exited.${NC}"
-
-echo -e "${ORANGE}Backing up space...${NC}"
-duply space backup
-echo -e "${GREEN}Duply exited.${NC}"
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
 
 echo -e "${ORANGE}Backing up development...${NC}"
 duply development backup
-echo -e "${GREEN}Duply exited.${NC}"
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
+
+echo -e "${ORANGE}Backing up lobby...${NC}"
+duply lobby backup
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
+
+echo -e "${ORANGE}Backing up global plugin directory...${NC}"
+duply plugindir backup
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
 
 echo -e "${ORANGE}Backing up RITcraft Survival...${NC}"
 duply rit-survival backup
-echo -e "${GREEN}Duply exited.${NC}"
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
+
+echo -e "${ORANGE}Backing up survival... (this could take a while)${NC}"
+duply survival backup
+echo -e "${GREEN}Duply exited.${NC}\n\n\n\n\n"
 
 echo -e "${GREEN}All done! Please double-check file integrity.${NC}"
