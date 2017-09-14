514b47c67d8e994f5b759bc75785539bf8ffc1c7
Justin W. Flory <git@jwf.io>
2017-09-13 23:39:42 -0400

    Wrap variable, remove typo

--- scripts/data/irc_logs_sync.sh
+++ scripts/data/irc_logs_sync.sh
@@ -34,12 +34,12 @@ while [[ $# -gt 1 ]]; do
     shift # past argument or value
 done
 
-if [ $DEFAULT = "YES" ]; then
+if [ "$DEFAULT" = "YES" ]; then
     echo "No arguments specified. Specify input directory, output directory, and chown user."
 fi
 
 # Make sure output directory exists
-if [ ! -d i"$OUTPUT_DIR" ]; then
+if [ ! -d "$OUTPUT_DIR" ]; then
     mkdir $OUTPUT_DIR
 fi
 
