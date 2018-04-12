50d2a7bb2a54b011f293b9488f3d8375777e1b68
Justin W. Flory <git@jwf.io>
2018-04-12 12:50:29 -0400

    Remove all WordPress, Minecraft, k8s playbooks (no longer in use nor needed)

--- playbooks/run_gsoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Handy shortcut for a command I run frequently...
-
-ansible-playbook -i ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/inventory -e ~/Software/GSoC/jflory7-ansible/playbooks/deliverables/inventory/group_vars/@wordpress -b ~/Software/GSoC/ansible/playbooks/manual/wordpress-install.yml --extra-vars="target='wordpress' wp_domain='b1.stg.derezzed.justinwflory.com:b2.stg.derezzed.justinwflory.com:b3.stg.derezzed.justinwflory.com:b4.stg.derezzed.justinwflory.com'"
