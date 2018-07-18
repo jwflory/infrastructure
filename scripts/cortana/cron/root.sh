488f3d31bf4af72295fef00548f0ca51ba5952d4
Justin W. Flory <git@jwf.io>
2018-07-17 19:23:17 -0500

    [cortana] Add SSL renewal script to root crontab
    This adds the new SSL renewal script to be run on the 15th of every
    month in the root user's crontab file.

--- scripts/cortana/cron/root.sh
+++ scripts/cortana/cron/root.sh
@@ -1,1 +1,2 @@
-0 4 * * * /home/jflory/git/infrastructure/scripts/cortana/irc_logs_sync_root.sh
+00 04 * * * /home/jflory/git/infrastructure/scripts/cortana/irc_logs_sync_root.sh
+00 05 15 * * /home/jflory/git/infrastructure/scripts/cortana/renew_ssl_certs.sh
